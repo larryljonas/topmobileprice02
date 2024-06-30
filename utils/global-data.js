baddddc483ee790022d2d0ded1ee079661976bab
larryljonas <larryljonas@gmail.com>
2024-06-30 07:02:36 +0500

    uptodate

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'TopMobileprices';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'TopMobileprices | Latest Mobile Prices and Specs in Pakistan';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
@@ -15,3 +15,4 @@ export const getGlobalData = () => {
     footerText,
   };
 };
+
