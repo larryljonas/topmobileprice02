c25512fead6d7d77974ea2bc55ce0b30408a72c0
larryljonas <larryljonas@gmail.com>
2024-06-30 07:08:33 +0500

    yes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'http://localhost:3000',
+    siteUrl: process.env.SITE_URL || 'https://topmobileprices.com/',
     generateRobotsTxt: true,
   };
   
