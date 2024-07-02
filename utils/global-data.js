67e9b9941e461be4cbfcbabf33473d4c2ddf7c99
larryljonas <larryljonas@gmail.com>
2024-07-02 14:08:30 +0500

    hhh

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'TopMobileprices';
+    : 'TopMobilePrices';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'TopMobileprices | Latest Mobile Prices and Specs in Pakistan';
+    : 'TopMobilePrices | Latest Mobile Prices and Specs in Pakistan';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
